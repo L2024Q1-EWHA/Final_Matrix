7500c032f0efe5d7acfc953b3e0d39fa85a4eb81
Hyekyung <88281367+gradeten@users.noreply.github.com>
2024-06-14 20:17:29 +0900

    홈 화면과 엔딩화면

--- Assets/Scripts/LoadSceneBtn.cs
+++ Assets/Scripts/LoadSceneBtn.cs
@@ -18,6 +18,11 @@ public class LoadSceneBtn : MonoBehaviour
         GameManager.Instance.GetComponent<LoadScene>().SceneChangeToPreviousScene();
     }
 
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
     /// <summary>
     /// Cover 화면에서 '시작하기' 버튼을 눌렀을 때, 진행 상황에 따라 '인트로', '홈화면', '방학 미션 화면'으로 이동
     /// </summary>
