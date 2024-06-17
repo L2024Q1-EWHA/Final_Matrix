d97f8110acabd47da2ecf4e2a4246a3fcd63db46
rnrgll <shuajjjjj@naver.com>
2024-06-18 03:19:12 +0900

    빌드 셋팅

--- Assets/Editor/ArrayElementTitleDrawer.cs
+++ Assets/Editor/ArrayElementTitleDrawer.cs
@@ -1,16 +1,6 @@
 using UnityEditor;
 using UnityEngine;
 
-//Attribute
-public class ArrayElementTitleAttribute : PropertyAttribute
-{
-    public string Varname;
-    public ArrayElementTitleAttribute(string ElementTitleVar)
-    {
-        Varname = ElementTitleVar;
-    }
-}
-
 [CustomPropertyDrawer(typeof(ArrayElementTitleAttribute))]
 public class ArrayElementTitleDrawer : PropertyDrawer
 {
---
+++ Assets/Scripts/ArrayElementTitleAttribute.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+// Attribute
+public class ArrayElementTitleAttribute : PropertyAttribute
+{
+    public string Varname;
+    public ArrayElementTitleAttribute(string ElementTitleVar)
+    {
+        Varname = ElementTitleVar;
+    }
+}
